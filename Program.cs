66ef4184399011fd705a084340ab2311bacd8bdd
Aaron K. Clark <akclark@atbashservices.com>
2022-07-14 03:10:09 -0500

    scaffolding

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+foreach (string str in args)
+{
+    if (str.Count() < 1)
+    {
+        string ErrorProgramNeedsVariables = "Error: name of pcap file must be supplied!";
+        Console.WriteLine(ErrorProgramNeedsVariables);
+    }
+
+    if (str.Count() > 1)
+    {
+        string ErrorProgramNeedsOnlyOneVariable = "Error: name of pcap file must be supplied!";
+        Console.WriteLine(ErrorProgramNeedsOnlyOneVariable);
+    }
+}
+string InfoProgramCompleted = "Program has completed sucessfully. Or unsuccessfully. What do I know? I'm a computer program!";
+Console.WriteLine(InfoProgramCompleted);
+
+string returnPFIX(string filename) {
+
+    string results = null;
+    return results;
+}
+
+string returnCSV(string filename)
+{
+
+    string results = null;
+    return results;
+}
